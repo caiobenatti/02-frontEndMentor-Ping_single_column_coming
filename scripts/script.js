2076e24689a1feae9c020a13de25158c48634176
Caio Benatti <caio.benatti@gmail.com>
2021-09-03 11:31:08 +0100

    final changes to layout, missing clearing the form if is valid

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,14 @@ const error = document.getElementById("error");
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 const checkEmail = () =>
-  form.value.match(mailformat) ? console.log("ok") : emailError();
+  form.value.match(mailformat) ? success() : emailError();
 
 const emailError = () => {
   error.style.display = "block";
   form.style.borderColor = "hsl(354, 100%, 66%)";
+};
+
+const success = () => {
+  error.style.display = "none";
+  form.style.borderColor = "hsl(223, 87%, 63%)";
 };
